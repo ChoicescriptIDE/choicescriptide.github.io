34f0926499f44646138e31502444814773b80018
Carey Williams <careyjwilliams@gmail.com>
2016-11-02 22:10:42 +0000

    Beta Update 1161

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -6,10 +6,10 @@
     "target": ""
 	},
 	"latest": {
-		"CSIDE_version": "0.0.0",
+		"CSIDE_version": "1.1.6.1",
     "nw_version": "0.18.2",
-    "desc": "",
-		"target": ""
+    "desc": "Quick patch: fixes case (in)sensitivity when finding scenes",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/1161.zip"
 	},
 	"dev": {
 		"CSIDE_version": "0.0.0",
