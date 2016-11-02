267afecfddfcee6fcce2a4301de7901a401f596e
Carey Williams <careyjwilliams@gmail.com>
2016-11-02 22:07:12 +0000

    CSIDE Version.js
    Used by the client to determine if there’s an update available

---
+++ downloads/updates/version.js
@@ -0,0 +1,20 @@
+{
+	"stable": {
+		"CSIDE_version": "0.0.0",
+    "nw_version": "0.18.2",
+    "desc": "",
+    "target": ""
+	},
+	"latest": {
+		"CSIDE_version": "0.0.0",
+    "nw_version": "0.18.2",
+    "desc": "",
+		"target": ""
+	},
+	"dev": {
+		"CSIDE_version": "0.0.0",
+    "nw_version": "0.18.2",
+    "desc": "",
+		"target": ""
+	}
+}
