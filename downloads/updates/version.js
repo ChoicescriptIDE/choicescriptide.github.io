30be1b77ab07ed7ef47cc5c024ff8bdfd13d9062
Carey Williams <careyjwilliams@gmail.com>
2016-11-04 03:07:20 +0000

    no updates

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -6,10 +6,10 @@
     "target": ""
 	},
 	"latest": {
-		"CSIDE_version": "1.8",
+		"CSIDE_version": "0.0.0",
     "nw_version": "0.18.2",
-    "desc": "YES, but no...",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/1161.zip"
+    "desc": "",
+		"target": ""
 	},
 	"dev": {
 		"CSIDE_version": "0.0.0",
