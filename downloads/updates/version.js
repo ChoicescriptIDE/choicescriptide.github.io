43240b53074c5ab9f40011dbc10cd3469a74f78a
Carey Williams <careyjwilliams@gmail.com>
2017-04-05 19:45:57 +0100

    Advertise 1001

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -6,10 +6,10 @@
     "target": ""
 	},
 	"latest": {
-		"CSIDE_version": "0.0.0",
-    "nw_version": "0.18.2",
-    "desc": "",
-		"target": ""
+		"CSIDE_version": "1.0.0.1",
+    "nw_version": "0.21.4",
+    "desc": "Font Fixes & Confirmation Boxes for Export Functions",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/1001.zip"
 	},
 	"dev": {
 		"CSIDE_version": "0.0.0",
