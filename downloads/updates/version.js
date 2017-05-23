cf13755153e48047326f9958db8ee33ef2c219bc
Carey Williams <careyjwilliams@gmail.com>
2017-05-23 15:30:54 +0100

    Version 1.0.0.4 Update

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -1,15 +1,15 @@
 {
 	"stable": {
-    "CSIDE_version": "1.0.0.2",
-    "nw_version": "0.21.4",
-    "desc": "Fix for missing editor functionality: Fullscreen, Search, AutoSuggest etc.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/1002.zip"
-	},
-	"latest": {
-		"CSIDE_version": "1.0.0.3",
+    "CSIDE_version": "1.0.0.3",
     "nw_version": "0.21.4",
     "desc": "ChoiceScript Update -- Support for Multireplace @{} etc.",
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/1003.zip"
+	},
+	"latest": {
+		"CSIDE_version": "1.0.0.4",
+    "nw_version": "0.21.4",
+    "desc": "1.0.0.4 - Fixes some web/dropbox version bugs and project re-ordering desync issues",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/1004.zip"
 	},
 	"dev": {
 		"CSIDE_version": "0.0.0",
