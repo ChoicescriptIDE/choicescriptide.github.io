5bf9d5a27b75cdb6e942b2dd3dd8166fbd372eaa
Carey Williams <careyjwilliams@gmail.com>
2017-12-18 01:30:27 +0000

    Release v1.2.0 to development channel

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -12,9 +12,9 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/110.zip"
 	},
 	"development": {
-		"CSIDE_version": "1.1.2",
+		"CSIDE_version": "1.2.0",
     "nw_version": "0.21.4",
-    "desc": "v1.1.2 - Latest ChoiceScript, deletion fix on macos, support for linux updates.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
+    "desc": "v1.2.0 - Feature release including custom themes, code folding and selection matching...",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/120.zip"
 	}
 }
