adc200da328d9edebe1aa9150e03f97bfa073763
Carey Williams <careyjwilliams@gmail.com>
2017-12-18 01:32:57 +0000

    Bump both stable and latest to v1.1.2
    1.1.2 has been use on Development for a long time now, hence pushing to stable as well.

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -1,15 +1,15 @@
 {
 	"stable": {
-    "CSIDE_version": "1.0.0.3",
+		"CSIDE_version": "1.1.2",
     "nw_version": "0.21.4",
-    "desc": "ChoiceScript Update -- Support for Multireplace @{} etc.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/1003.zip"
+    "desc": "v1.1.2 - Deletion fix on macos, support for linux updates.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
 	},
 	"latest": {
-		"CSIDE_version": "1.1.0",
+		"CSIDE_version": "1.1.2",
     "nw_version": "0.21.4",
-    "desc": "v1.1.0 - Additional aesthetic options, latest version of ChoiceScript, bug fixes and usability tweaks.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/110.zip"
+    "desc": "v1.1.2 - Deletion fix on macos, support for linux updates.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
 	},
 	"development": {
 		"CSIDE_version": "1.2.0",
