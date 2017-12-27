c4bd65004412e6f79ba8a216d53ccf9863027bf7
Carey Williams <careyjwilliams@gmail.com>
2017-12-27 01:28:32 +0000

    121

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -12,9 +12,9 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
 	},
 	"development": {
-		"CSIDE_version": "1.2.0",
+		"CSIDE_version": "1.2.1",
     "nw_version": "0.21.4",
-    "desc": "v1.2.0 - Feature release including custom themes, code folding and selection matching...",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/120.zip"
+    "desc": "v1.2.1 - Provides a criticial bug fix related to persistent sessions",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	}
 }
