9153118e8bea13cae622d0ec6b608f9c57e7a235
Carey Williams <careyjwilliams@gmail.com>
2017-12-27 01:40:21 +0000

    Critical 113 fix for stable+latest

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -1,15 +1,15 @@
 {
 	"stable": {
-		"CSIDE_version": "1.1.2",
+		"CSIDE_version": "1.1.3",
     "nw_version": "0.21.4",
-    "desc": "v1.1.2 - Deletion fix on macos, support for linux updates.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
+    "desc": "v1.1.3 - Provides a criticial bug fix related to persistent sessions",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/113.zip"
 	},
 	"latest": {
-		"CSIDE_version": "1.1.2",
+		"CSIDE_version": "1.1.3",
     "nw_version": "0.21.4",
-    "desc": "v1.1.2 - Deletion fix on macos, support for linux updates.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/112.zip"
+    "desc": "v1.1.3 - Provides a criticial bug fix related to persistent sessions",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/113.zip"
 	},
 	"development": {
 		"CSIDE_version": "1.2.1",
