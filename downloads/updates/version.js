0ecb953675e331179a003983da71a76ae0f6fe32
Carey Williams <careyjwilliams@gmail.com>
2018-01-19 00:52:06 +0000

    Add accessibility channel
    Add an accessibility channel so that users can test accessibility updates. These will be merged back into the other channels/versions eventually.

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -16,5 +16,11 @@
     "nw_version": "0.21.4",
     "desc": "v1.2.1 - Provides a criticial bug fix related to persistent sessions",
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
-	}
+	},
+  "accessible": {
+    "CSIDE_version": "1.1.2",
+    "nw_version": "0.21.4",
+    "desc": "",
+    "target": ""
+  }
 }
