0f30f6412cf29282c2533d7f3a1901e95711994f
Carey Williams <careyjwilliams@gmail.com>
2018-03-25 18:26:13 +0100

    Bump 'latest' channel to 1.2.1
    This is a feature release bump, hence describing 1.2.0, not 1.2.1 (as Latest already had the 1.1.3 critical patch).

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -6,15 +6,15 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/113.zip"
 	},
 	"latest": {
-		"CSIDE_version": "1.1.3",
+		"CSIDE_version": "1.2.1",
     "nw_version": "0.21.4",
-    "desc": "v1.1.3 - Provides a criticial bug fix related to persistent sessions",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/113.zip"
+    "desc": "v1.2.1 - Provides a criticial bug fix related to persistent sessions",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
 	"development": {
 		"CSIDE_version": "1.2.1",
     "nw_version": "0.21.4",
-    "desc": "v1.2.1 - Provides a criticial bug fix related to persistent sessions",
+    "desc": "v1.2.1 - (1.2.0) Feature release: Custom themes, code folding and more.",
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
   "accessible": {
