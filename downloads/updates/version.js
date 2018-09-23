ad8a562b19572eb017e60abe467c4e7212f55412
Carey Williams <CareyJWilliams@users.noreply.github.com>
2018-09-23 17:45:41 +0100

    Bump v1.2.1 to stable channel

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -1,9 +1,9 @@
 {
 	"stable": {
-		"CSIDE_version": "1.1.3",
+		"CSIDE_version": "1.2.1",
     "nw_version": "0.21.4",
-    "desc": "v1.1.3 - Provides a criticial bug fix related to persistent sessions",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/113.zip"
+    "desc": "v1.2.1 - (1.2.0) Feature release: Custom themes, code folding and more.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
 	"latest": {
 		"CSIDE_version": "1.2.1",
