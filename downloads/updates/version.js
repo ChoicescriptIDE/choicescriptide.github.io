2355b5383501e705d0f4924d6a8fabf2a92917da
Carey Williams <careyjwilliams@gmail.com>
2019-06-02 23:58:20 +0100

    Revert "Enable v1.3.0 on the development channel"
    This reverts commit a252c7d19a9149b8c1d9bc0ec4788ae1bb3c6b22.

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -12,10 +12,10 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
 	"development": {
-		"CSIDE_version": "1.3.0",
+		"CSIDE_version": "1.2.1",
     "nw_version": "0.21.4",
-    "desc": "v1.3.0 Feature release: User dictionary improvements, custom themes and bug fixes.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/130.zip"
+    "desc": "v1.2.1 - (1.2.0) Feature release: Custom themes, code folding and more.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
   "accessible": {
     "CSIDE_version": "1.1.2",
