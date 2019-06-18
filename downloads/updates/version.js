4a0ddf113c7aa56e1e98bdd7cb49a185641b90b6
Carey Williams <careyjwilliams@gmail.com>
2019-06-18 23:18:29 +0100

    Push 1.3.0 to latest channel.

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -6,15 +6,15 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
 	},
 	"latest": {
-		"CSIDE_version": "1.2.1",
+		"CSIDE_version": "1.3.0",
     "nw_version": "0.21.4",
-    "desc": "v1.2.1 - (1.2.0) Feature release: Custom themes, code folding and more.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
+    "desc": "v1.3.0 Feature release: User dictionary, custom theme improvements, and bug fixes.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/130.zip"
 	},
 	"development": {
 		"CSIDE_version": "1.3.0",
     "nw_version": "0.21.4",
-    "desc": "v1.3.0 Feature release: User dictionary improvements, custom themes and bug fixes.",
+    "desc": "v1.3.0 Feature release: User dictionary, custom theme improvements, and bug fixes.",
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/130.zip"
 	},
   "accessible": {
