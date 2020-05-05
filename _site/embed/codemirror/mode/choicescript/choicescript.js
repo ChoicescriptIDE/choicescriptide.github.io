3c49df117ad7e900ad431ecc609e4410c4cf2b08
Carey Williams <careyjwilliams@gmail.com>
2020-05-05 01:40:12 +0100

    Host embeddable IDE

---
+++ _site/embed/codemirror/mode/choicescript/choicescript.js
@@ -0,0 +1,316 @@
+CodeMirror.defineMode("choicescript", function(conf, parserConf) {
+    var ERRORCLASS = 'error';
+
+    function wordRegexp(words) {
+        return new RegExp("^((" + words.join(")|(") + "))\\b");
+    }
+	
+	function CommandRegexp(words) {
+		return new RegExp("^\\*(" + words.join('|') + ")");
+	}
+
+    var singleOperators = parserConf.singleOperators || new RegExp("^[\\+\\-\/%&|\\^~<>#&$=\*]");
+    var singleDelimiters = parserConf.singleDelimiters || new RegExp('^[\\(\\)\\[\\]\\{\\}@,`=\\.]');
+    var doubleOperators = parserConf.doubleOperators || new RegExp("^((==)|(!=)|(<=)|(>=)|(<>)|(<<)|(>>)|(//)|(\\*\\*))");
+    var doubleDelimiters = parserConf.doubleDelimiters || new RegExp("^((\\+=)|(\\-=)|(\\*=)|(%=)|(/=)|(&=)|(\\|=)|(\\^=))");
+    var tripleDelimiters = parserConf.tripleDelimiters || new RegExp("^((//=)|(>>=)|(<<=)|(\\*\\*=))");
+    var identifiers = parserConf.identifiers|| new RegExp("^[_A-Za-z][_A-Za-z0-9]*");
+
+    var wordOperators = wordRegexp(['and', 'or', 'not']);
+    var commonkeywords = ['/^\*label /'];
+    var commonCommands = ["achieve", "allow\_reuse", "choice", "create", "delete", "disable\_reuse", "elseif", "else", "elsif",
+						  "choice", "ending", "finish", "fake\_choice", "gosub", "goto_scene", "gotoref", "goto", "goto_random_scene",
+						  "hide\_reuse", "if", "input\_text", "input\_number", "image", "line\_break", "link",
+						  "more\_games", "page\_break", "print", "rand", "return", "scene_list", "setref", "set", "share\_this\_game", "sound", "stat\_chart", 
+						  "selectable\_if", "title", "temp"];
+	var specialCommands = ["abort", "advertisement", "check_purchase", "delay_break", "delay_ending", "looplimit", "restore_purchases", "restart",
+						   "restore_game", "script", "show_password", "save_game", "subscribe"];
+	var commentWords = ["comment"];
+	var indentCommands = ["choice", "if", "scene_list", "elseif", "else", "elsif", "fake_choice", "stat_chart"];
+	var dedentCommands = ["finish", "goto_scene", "goto", "ending"];
+	
+	
+/*     if (!!parserConf.version && parseInt(parserConf.version, 10) === 3) {
+        var stringPrefixes = new RegExp("^(([rb]|(br))?('{3}|\"{3}|['\"]))", "i");
+    } else {
+        var stringPrefixes = new RegExp("^(([rub]|(ur)|(br))?('{3}|\"{3}|['\"]))", "i");
+    } */
+	
+    var keywords = /^\*label| $/;
+
+    var builtins = CommandRegexp(commonCommands.concat(specialCommands));
+	var comments = CommandRegexp(commentWords);
+	var indentCommands = CommandRegexp(indentCommands);
+	var dedentCommands = CommandRegexp(dedentCommands);
+    var indentInfo = null;
+
+    // tokenizers
+    function tokenBase(stream, state) {
+	
+		var ch = stream.peek();
+		
+        // Handle scope changes (sol = start of line)
+         if (stream.sol()) {
+            var scopeOffset = state.scopes[0].offset;
+            if (stream.eatSpace()) {
+                var lineOffset = stream.indentation();
+                if (lineOffset > scopeOffset) {
+                    indentInfo = 'indent';
+                } else if (lineOffset < scopeOffset) {
+                    indentInfo = 'dedent';
+                }
+                return null;
+            } else {
+                if (scopeOffset > 0) {
+                    dedent(stream, state);
+                }
+            }
+        }
+        if (stream.eatSpace()) {
+            return null;
+        } 
+		
+        // Handle Comments
+		if (stream.match(comments)) {
+			stream.skipToEnd();
+            return 'comment';
+		}
+
+        // Handle Strings (disabled for now)
+        // if (stream.match(stringPrefixes)) {
+            // state.tokenize = tokenStringFactory(stream.current());
+            // return state.tokenize(stream, state);
+        // }
+
+        // Handle operators and Delimiters
+        if (stream.match(tripleDelimiters) || stream.match(doubleDelimiters)) {
+            return null;
+        }
+		if (stream.match(builtins)) {
+            return 'builtin';
+        }
+        if (stream.match(keywords)) {
+            return 'keyword';
+        }
+        if (stream.match(doubleOperators) || stream.match(singleOperators)) { // || stream.match(wordOperators)
+            return 'operator';
+        }
+        if (stream.match(singleDelimiters)) {
+            return null;
+        }
+
+        if (stream.match(identifiers)) {
+            if (state.lastToken == 'def' || state.lastToken == 'class') {
+                return 'def';
+            }
+            //return 'variable';
+			return null;
+        }
+
+        // Handle non-detected items
+        stream.next();
+        return null; //could be ERRORCLASS
+    }
+
+    function tokenStringFactory(delimiter) {
+        while ('rub'.indexOf(delimiter.charAt(0).toLowerCase()) >= 0) {
+            delimiter = delimiter.substr(1);
+        }
+        var singleline = delimiter.length == 1;
+        var OUTCLASS = 'string';
+
+/*         function tokenString(stream, state) {
+            while (!stream.eol()) {
+                stream.eatWhile(/[^'"\\]/);
+                if (stream.eat('\\')) {
+                    stream.next();
+                    if (singleline && stream.eol()) {
+                        return OUTCLASS;
+                    }
+                } else if (stream.match(delimiter)) {
+                    state.tokenize = tokenBase;
+                    return OUTCLASS;
+                } else {
+                    stream.eat(/['"]/);
+                }
+            }
+            if (singleline) {
+                if (parserConf.singleLineStringErrors) {
+                    return ERRORCLASS;
+                } else {
+                    state.tokenize = tokenBase;
+                }
+            }
+            return OUTCLASS;
+        } */
+        tokenString.isString = true;
+        return tokenString;
+    }
+
+    function indent(stream, state, type) {
+        type = type || 'py';
+        var indentUnit = 0;
+        if (type === 'py') {
+            if (state.scopes[0].type !== 'py') {
+                state.scopes[0].offset = stream.indentation();
+                return;
+            }
+            for (var i = 0; i < state.scopes.length; ++i) {
+                if (state.scopes[i].type === 'py') {
+                    indentUnit = state.scopes[i].offset + conf.indentUnit;
+                    break;
+                }
+            }
+        } else {
+            indentUnit = stream.column() + stream.current().length;
+        }
+        state.scopes.unshift({
+            offset: indentUnit,
+            type: type
+        });
+    }
+
+     function dedent(stream, state, type) {
+        type = type || 'py';
+        if (state.scopes.length == 1) return;
+        if (state.scopes[0].type === 'py') {
+            var _indent = stream.indentation();
+            var _indent_index = -1;
+            for (var i = 0; i < state.scopes.length; ++i) {
+                if (_indent === state.scopes[i].offset) {
+                    _indent_index = i;
+                    break;
+                }
+            }
+            if (_indent_index === -1) {
+                return true;
+            }
+            while (state.scopes[0].offset !== _indent) {
+                state.scopes.shift();
+            }
+            return false;
+        } else {
+            if (type === 'py') {
+                state.scopes[0].offset = stream.indentation();
+                return false;
+            } else {
+                if (state.scopes[0].type != type) {
+                    return true;
+                }
+                state.scopes.shift();
+                return false;
+            }
+        }
+    } 
+
+    function tokenLexer(stream, state) {
+        indentInfo = null;
+        var style = state.tokenize(stream, state);
+        var current = stream.current();
+
+        // Handle decorators
+        if ((style === 'variable' || style === 'builtin')
+            && state.lastStyle === 'meta') {
+            style = 'meta';
+        }
+
+        // Handle scope changes.
+/*      if (current === 'pass' || current === 'return') {
+            state.dedent += 1;
+        } */
+		if (current.match(/#/)) {
+			var moreHashes = stream.skipTo("#");
+			if (!moreHashes) {
+				indentInfo = 'indent'; 
+			}
+		}
+        if (current.match(indentCommands)) {// stream.eol() is a crap way of ensuring multiple commands (on one line) only result in a single indent
+			if (current.match(/if/)) {
+				var isChoice = stream.skipTo("#"); //prevent double indent when using *if with *choice
+				if (!isChoice) indentInfo = 'indent';
+			} else {
+				indentInfo = 'indent';
+			}
+		}
+		if (indentInfo === 'indent') {
+			//state.dedent -= 1;
+			indent(stream, state);
+		}
+		else if (current.match(dedentCommands)) {  //stream.eol() see .match(indentCommands)
+			state.dedent += 1; //DEDENT
+		}
+        var delimiter_index = '('.indexOf(current);
+        if (delimiter_index !== -1) {
+            indent(stream, state, ')'.slice(delimiter_index, delimiter_index+1));
+        }
+        if (indentInfo === 'dedent') {
+            if (dedent(stream, state)) {
+                return ERRORCLASS;
+            }
+        }
+        delimiter_index = ')'.indexOf(current);
+        if (delimiter_index !== -1) {
+            if (dedent(stream, state, current)) {
+                return ERRORCLASS;
+            }
+        }
+        if (state.dedent > 0 && stream.eol() && state.scopes[0].type == 'py') {
+            if (state.scopes.length > 1) state.scopes.shift();
+            state.dedent -= 1;
+        }
+        return style;
+    }
+
+    var external = {
+        startState: function(basecolumn) {
+            return {
+              tokenize: tokenBase,
+              scopes: [{offset:basecolumn || 0, type:'py'}],
+              lastStyle: null,
+              lastToken: null,
+              lambda: false,
+              dedent: 0
+          };
+        },
+
+        token: function(stream, state) {
+            var style = tokenLexer(stream, state);
+
+            state.lastStyle = style;
+
+            var current = stream.current();
+            if (current && style) {
+                state.lastToken = current;
+            }
+
+            if (stream.eol() && state.lambda) {
+                state.lambda = false;
+            }
+            return style;
+        },
+
+        indent: function(state) {
+            if (state.tokenize != tokenBase) {
+                return state.tokenize.isString ? CodeMirror.Pass : 0;
+            }
+
+            return state.scopes[0].offset;
+        },
+
+        lineComment: "*comment ",
+        fold: "indent"
+    };
+    return external;
+});
+
+CodeMirror.defineMIME("text/x-choicescript", "choicescript");
+
+(function() {
+  "use strict";
+  var words = function(str){return str.split(' ');};
+
+  CodeMirror.defineMIME("text/x-choice", {
+    name: "choicescript",
+    extra_keywords: words("")
+  });
+})();
---
+++ _site/embed/codemirror/mode/haskell/haskell.js
@@ -0,0 +1,223 @@
+CodeMirror.defineMode("haskell", function(_config, modeConfig) {
+
+  function switchState(source, setState, f) {
+    setState(f);
+    return f(source, setState);
+  }
+
+  // These should all be Unicode extended, as per the Haskell 2010 report
+  var smallRE = /[a-z_]/;
+  var largeRE = /[A-Z]/;
+  var digitRE = /[0-9]/;
+  var hexitRE = /[0-9A-Fa-f]/;
+  var octitRE = /[0-7]/;
+  var idRE = /[a-z_A-Z0-9']/;
+  var symbolRE = /[-!#$%&*+.\/<=>?@\\^|~:]/;
+  var specialRE = /[(),;[\]`{}]/;
+  var whiteCharRE = /[ \t\v\f]/; // newlines are handled in tokenizer
+
+  function normal(source, setState) {
+    if (source.eatWhile(whiteCharRE)) {
+      return null;
+    }
+
+    var ch = source.next();
+    if (specialRE.test(ch)) {
+      if (ch == '{' && source.eat('-')) {
+        var t = "comment";
+        if (source.eat('#')) {
+          t = "meta";
+        }
+        return switchState(source, setState, ncomment(t, 1));
+      }
+      return null;
+    }
+
+    if (ch == '\'') {
+      if (source.eat('\\')) {
+        source.next();  // should handle other escapes here
+      }
+      else {
+        source.next();
+      }
+      if (source.eat('\'')) {
+        return "string";
+      }
+      return "error";
+    }
+
+    if (ch == '"') {
+      return switchState(source, setState, stringLiteral);
+    }
+
+    if (largeRE.test(ch)) {
+      source.eatWhile(idRE);
+      if (source.eat('.')) {
+        return "qualifier";
+      }
+      return null;
+    }
+
+    if (smallRE.test(ch)) {
+      source.eatWhile(idRE);
+      return "variable";
+    }
+
+    if (digitRE.test(ch)) {
+      if (ch == '0') {
+        if (source.eat(/[xX]/)) {
+          source.eatWhile(hexitRE); // should require at least 1
+          return "integer";
+        }
+        if (source.eat(/[oO]/)) {
+          source.eatWhile(octitRE); // should require at least 1
+          return "number";
+        }
+      }
+      source.eatWhile(digitRE);
+      var t = "number";
+      if (source.eat('.')) {
+        t = "number";
+        source.eatWhile(digitRE); // should require at least 1
+      }
+      if (source.eat(/[eE]/)) {
+        t = "number";
+        source.eat(/[-+]/);
+        source.eatWhile(digitRE); // should require at least 1
+      }
+      return t;
+    }
+
+    if (symbolRE.test(ch)) {
+      if (ch == '*' && source.eat(/c/) && source.eat(/o/) && source.eat(/m/) && source.eat(/m/) && source.eat(/e/) && source.eat(/n/) && source.eat(/t/)) {
+        source.eatWhile(/-/);
+        if (!source.eat(symbolRE)) {
+          source.skipToEnd();
+          return "comment";
+        }
+      }
+      var t = "variable";
+      if (ch == ':') {
+        t = "variable-2";
+      }
+      source.eatWhile(symbolRE);
+      return t;
+    }
+
+    return "error";
+  }
+
+  function ncomment(type, nest) {
+    if (nest == 0) {
+      return normal;
+    }
+    return function(source, setState) {
+      var currNest = nest;
+      while (!source.eol()) {
+        var ch = source.next();
+        if (ch == '{' && source.eat('-')) {
+          ++currNest;
+        }
+        else if (ch == '-' && source.eat('}')) {
+          --currNest;
+          if (currNest == 0) {
+            setState(normal);
+            return type;
+          }
+        }
+      }
+      setState(ncomment(type, currNest));
+      return type;
+    };
+  }
+
+  function stringLiteral(source, setState) {
+    while (!source.eol()) {
+      var ch = source.next();
+      if (ch == '"') {
+        setState(normal);
+        return "string";
+      }
+      if (ch == '\\') {
+        if (source.eol() || source.eat(whiteCharRE)) {
+          setState(stringGap);
+          return "string";
+        }
+        if (source.eat('&')) {
+        }
+        else {
+          source.next(); // should handle other escapes here
+        }
+      }
+    }
+    setState(normal);
+    return "error";
+  }
+
+  function stringGap(source, setState) {
+    if (source.eat('\\')) {
+      return switchState(source, setState, stringLiteral);
+    }
+    source.next();
+    setState(normal);
+    return "error";
+  }
+
+
+  var wellKnownWords = (function() {
+    var wkw = {};
+    function setType(t) {
+      return function () {
+        for (var i = 0; i < arguments.length; i++)
+          wkw[arguments[i]] = t;
+      };
+    }
+
+    // setType("keyword")(
+      // "case", "class", "data", "default", "deriving", "do", "else", "foreign",
+      // "if", "import", "in", "infix", "infixl", "infixr", "instance", "let",
+      // "module", "newtype", "of", "then", "type", "where", "_");
+
+    setType("keyword")(
+      "\.\.", ":", "::", "=", "\\", "\"", "<-", "->", "@", "~", "=>", "*", '&');
+
+    setType("builtin")(
+      "!!", "$!", "$", "&&", "+", "++", "-", ".", "/", "/=", "<", "<=", "=<<",
+      "==", ">", ">=", ">>", ">>=", "^", "^^", "||", "*", "**");
+
+    setType("builtin")("False","True");
+
+    setType("builtin")(
+      "achieve", "allow\_reuse", "choice", "create", "delete", "disable\_reuse", "else", "elseif", "elsif",
+	  "choice", "ending", "finish", "fake\_choice", "gosub", "goto_scene", "goto", "goto_random_scene",
+	  "hide\_reuse", "if", "input\_text", "input\_number", "image", "label", "line\_break", "link",
+	  "more\_games", "page\_break", "print", "rand", "return", "set", "share\_this\_game", "stat\_chart", 
+	  "selectable\_if", "temp");
+
+    var override = modeConfig.overrideKeywords;
+    if (override) for (var word in override) if (override.hasOwnProperty(word))
+      wkw[word] = override[word];
+
+    return wkw;
+  })();
+
+
+
+  return {
+    startState: function ()  { return { f: normal }; },
+    copyState:  function (s) { return { f: s.f }; },
+
+    token: function(stream, state) {
+      var t = state.f(stream, function(s) { state.f = s; });
+      var w = stream.current();
+      return (w in wellKnownWords) ? wellKnownWords[w] : t;
+    },
+
+    blockCommentStart: "{-",
+    blockCommentEnd: "-}",
+    lineComment: "--"
+  };
+
+});
+
+CodeMirror.defineMIME("text/x-haskell", "haskell");
