755817ad29f5245d7bb81efb006d8a64cb26407e
Carey Williams <careyjwilliams@gmail.com>
2020-08-09 12:26:07 +0100

    Enable v1.3.1 on 'Stable'

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -1,9 +1,9 @@
 {
 	"stable": {
-		"CSIDE_version": "1.2.1",
+		"CSIDE_version": "1.3.1",
     "nw_version": "0.21.4",
-    "desc": "v1.2.1 - (1.2.0) Feature release: Custom themes, code folding and more.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/121.zip"
+    "desc": "v1.3.X - Bug fixes and Improvements to User Dictionary + Custom Theme and more.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/131.zip"
 	},
 	"latest": {
     "CSIDE_version": "1.3.1",
