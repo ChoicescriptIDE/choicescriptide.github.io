ca33a2e7cb2df1e0ab25ef431f22805a73b7383b
Carey Williams <CareyJWilliams@users.noreply.github.com>
2022-06-27 02:09:43 +0100

    Publish v1.3.2

--- downloads/updates/version.js
+++ downloads/updates/version.js
@@ -12,10 +12,10 @@
 		"target": "https://choicescriptide.github.io/downloads/updates/targets/131.zip"
 	},
 	"development": {
-    "CSIDE_version": "1.3.1",
+    "CSIDE_version": "1.3.2",
     "nw_version": "0.21.4",
-    "desc": "v1.3.1 Hotfix: reverts changes causing corrupt metadata.",
-		"target": "https://choicescriptide.github.io/downloads/updates/targets/131.zip"
+    "desc": "v1.3.2 Patch release: upgrades ChoiceScript.",
+		"target": "https://choicescriptide.github.io/downloads/updates/targets/132.zip"
 	},
   "accessible": {
     "CSIDE_version": "1.1.2",
